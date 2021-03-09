a0eea85c95db0dac2885cf157b41907f9676482e
IvanAguado22 <100405871@alumnos.uc3m.es>
2021-03-09 17:45:20 +0100

    Proyecto terminado

--- src/lex/manual/Driver.java
+++ src/lex/manual/Driver.java
@@ -12,11 +12,12 @@ import java.util.ArrayList;
 
 class Driver {
 	public static void main(String args[]) throws IOException {
-		// Entrada de datos: teclado por defecto, fichero si hay argumento
-		// InputStream dataStream = System.in;
-		FileInputStream dataStream = new FileInputStream(args[0]);
+		// Entrada de datos: fichero especificado en los argumentos
+		FileInputStream dataStreamBuffer = new FileInputStream(args[0]);
+		FileInputStream dataStreamScanner = new FileInputStream(args[0]);
+
 		System.out.println("Leyendo entrada de fichero... ");
-		InputStreamReader isr = new InputStreamReader(dataStream, "utf8");
+		InputStreamReader isr = new InputStreamReader(dataStreamBuffer, "utf8");
 		BufferedReader br = new BufferedReader(isr);
 
 		String linea;
@@ -27,21 +28,19 @@ class Driver {
 			caracteres += linea.length();
 		}
 
-		FileInputStream dataStream1 = new FileInputStream(args[0]);
-		// Creamos el objeto scanner
-		Lexer scanner = new Lexer(dataStream1);
+		// Creamos el objeto scanner y los arraylist para cada tipo de simbolo
+		Lexer scanner = new Lexer(dataStreamScanner);
 		ArrayList<Symbol> operadores = new ArrayList<Symbol>();
 		ArrayList<Symbol> separadores = new ArrayList<Symbol>();
 		ArrayList<Symbol> numeros = new ArrayList<Symbol>();
 		ArrayList<Symbol> nombres = new ArrayList<Symbol>();
 		ArrayList<Symbol> emails = new ArrayList<Symbol>();
-		ArrayList<Symbol> DNIs = new ArrayList<Symbol>();
+		ArrayList<Symbol> DNI = new ArrayList<Symbol>();
 		ArrayList<Symbol> matriculas = new ArrayList<Symbol>();
 		ArrayList<Symbol> fechas = new ArrayList<Symbol>();
 
-		// Mientras no alcancemos el fin de la entrada
+		// Mientras no alcancemos el fin del fichero
 		boolean end = false;
-
 		while (!end) {
 			try {
 				Symbol token = scanner.next_token();
@@ -57,7 +56,7 @@ class Driver {
 				} else if (token.type() == 13) {
 					emails.add(token);
 				} else if (token.type() == 14) {
-					DNIs.add(token);
+					DNI.add(token);
 				} else if (token.type() == 15) {
 					matriculas.add(token);
 				} else if (token.type() == 16) {
@@ -68,14 +67,17 @@ class Driver {
 				x.printStackTrace();
 			}
 		}
+		
 		operadores.trimToSize();
 		separadores.trimToSize();
 		numeros.trimToSize();
 		nombres.trimToSize();
 		emails.trimToSize();
-		DNIs.trimToSize();
+		DNI.trimToSize();
 		matriculas.trimToSize();
 		fechas.trimToSize();
+		
+		// Impresion por pantalla
 		System.out.println("OPERADORES: ");
 		if (operadores.isEmpty()) {
 			System.out.println("No se han encontrado operadores");
@@ -85,7 +87,7 @@ class Driver {
 			}
 			System.out.println();
 		}
-		System.out.println("SEPARADORES Y PARENTESIS: : ");
+		System.out.println("SEPARADORES Y PARENTESIS:  ");
 		if (separadores.isEmpty()) {
 			System.out.println("No se han encontrado separadores ni parentesis");
 		} else {
@@ -109,7 +111,7 @@ class Driver {
 			System.out.println("No se han encontrado nombres");
 		} else {
 			for (int i = 0; i < nombres.size(); i++) {
-				System.out.print(nombres.get(i).value() + ", ");
+				System.out.print(nombres.get(i).value() + " ");
 			}
 			System.out.println();
 		}
@@ -118,16 +120,16 @@ class Driver {
 			System.out.println("No se han encontrado emails");
 		} else {
 			for (int i = 0; i < emails.size(); i++) {
-				System.out.print(emails.get(i).value() + ", ");
+				System.out.print(emails.get(i).value() + " ");
 			}
 			System.out.println();
 		}
 		System.out.print("DNI: ");
-		if (DNIs.isEmpty()) {
-			System.out.println("No se han encontrado DNIs");
+		if (DNI.isEmpty()) {
+			System.out.println("No se han encontrado DNI");
 		} else {
-			for (int i = 0; i < DNIs.size(); i++) {
-				System.out.print(DNIs.get(i).value() + ", ");
+			for (int i = 0; i < DNI.size(); i++) {
+				System.out.print(DNI.get(i).value() + " ");
 			}
 			System.out.println();
 		}
@@ -136,7 +138,7 @@ class Driver {
 			System.out.println("No se han encontrado matriculas");
 		} else {
 			for (int i = 0; i < matriculas.size(); i++) {
-				System.out.print(matriculas.get(i).value() + ", ");
+				System.out.print(matriculas.get(i).value() + " ");
 			}
 			System.out.println();
 		}
@@ -145,15 +147,15 @@ class Driver {
 			System.out.println("No se han encontrado nombres");
 		} else {
 			for (int i = 0; i < fechas.size(); i++) {
-				System.out.print(fechas.get(i).value() + ", ");
+				System.out.print(fechas.get(i).value() + " ");
 			}
 			System.out.println();
 		}
 		System.out.println("EXTENSION: ");
 		System.out.println("Numero de lineas: " + lineas);
 		System.out.println("Numero de caracteres: " + caracteres);
-		int simbolos = operadores.size() + separadores.size() + numeros.size()
-		+ nombres.size() + emails.size() + DNIs.size() + matriculas.size() + fechas.size();
+		int simbolos = operadores.size() + separadores.size() + numeros.size() + nombres.size() + emails.size()
+				+ DNI.size() + matriculas.size() + fechas.size();
 		System.out.println("Numero de simbolos: " + simbolos);
 
 		System.out.println("\n\n -- Bye-bye -- ");
